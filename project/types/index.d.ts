ce030ef4a5b498d2e8a1f7e2aa10a2a2356e22e8
TonyPythoneer <tonypythoneer@gmail.com>
2017-02-08 12:46:57 +0800

    Don't ignore type file

---
+++ project/types/index.d.ts
@@ -0,0 +1,11 @@
+import * as bluebird from 'bluebird';
+
+
+//  Overwrite the original Promise of the definition
+declare class Promise<R> extends bluebird<R> {}
+declare namespace NodeJS {
+    export interface Global {
+        Promise: typeof Promise;
+    }
+}
+declare var global: NodeJS.Global;
